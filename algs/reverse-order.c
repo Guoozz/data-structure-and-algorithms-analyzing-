2c37e97cd6fc8cd5258a38562bdcdf7bc41b4388
Guoozz <csguozhen@gmail.com>
2015-09-24 12:20:39 +0800

    快排和求逆序对

--- algs/reverse-order.c
+++ algs/reverse-order.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<stdlib.h>
 int counts = 0;
 #define len(x) sizeof((x)) / sizeof(int)
 
@@ -14,24 +15,25 @@ int counts = 0;
    merge-counts(a,p,q,r)
 
 
- merge-counts(a,p,q,r)
+
+ merge-counts(a,p,q,r) //
   n1 = q - p + 1
   n2 = r - q
   l[n1] = a[p,q]
   r[n2] = a[q+1,r]
   i = j = 1
-  k = p - 1
+  k = p
   counts = 0  逆序对的数目
  该迭代的不变式 :a[p,k] 已排好序，l[i],r[j]分别是各自数组中最小的元素
-
- while i > n1 or j > n2 // l 或 r 数组其中一个遍历完就结束循环
+ i = j = 0
+ while i > n1 and j > n2 // l 或 r 数组其中一个遍历完就结束循环
       if l[i] <= r[j]   // 对于 l[i] <= r[j] 肯定不存在逆序对,
-         a[++k] = l[i]
+         a[k++] = l[i]
          ++i
       else             // 对于 l[i] > r[j] ，满足逆序对的条件
-         a[++k] = r[j]
+         a[k++] = r[j]
          j++
-         counts++
+         counts = n1 - i;
 
  if i > n1
    while j <= n2
@@ -39,21 +41,23 @@ int counts = 0;
  else
   while i <= n1
    a[++k] =  l[i]
-   counts += n2   //假设 l = {8,9} r = {1,2,3,4,5} a = l + r 则 a 中存在 2 × 5 = 10 个逆序对
+
  return counts
 
  */
 
 int merge_counts(int a[],int p,int q,int r);
+void print_array(int a[],int p,int r);
 
 int reverse_order(int a[],int p,int r)
 {
   if(p < r){
     int q = (p + r) / 2;
     reverse_order(a,p,q);
-    reverse_order(a,q+1,r);
+    reverse_order(a,q + 1,r);
     return  merge_counts(a,p,q,r);
   }
+  return 0;
 }
 
 int merge_counts(int a[],int p,int q,int r)
@@ -67,33 +71,53 @@ int merge_counts(int a[],int p,int q,int r)
     left[i] = a[i + p];
   for(i = 0;i < n2;++i)
     right[i] = a[i + q + 1];
-  int k = p - 1;
-  while(i < n1 || j < n2){
+  int k = p;
+  i = j = 0;
+  while(i < n1 && j < n2){
     if(left[i] <= right[j])
-      a[++k] = left[i++];
+      a[k++] = left[i++];
     else{
-      a[++k] = right[j++];
-      ++counts;
+      a[k++] = right[j++];
+      counts += n1 - i;  //n1 - i 为 left数组中剩余的元素
     }
   }
-  if(i >= n1){
-    while(j < n2)
-      a[++k] = right[j++];
-  }
-  else
-    while(i < n1){
-      a[++k] = left[i++];
-      counts += n2;
-    }
+  while(j < n2)
+      a[k++] = right[j++];
+  while(i < n1)
+      a[k++] = left[i++];
   return counts;
+}
+
+void print_array(int a[],int p,int r)
+{
+  for(int i = p;i <= r;++i)
+    {
+      printf(" %d,",a[i]);
+    }
+  printf("\n");
+}
+
+
+/* 直接发求逆序对 */
+int r(int a[],int hi)
+{
+  int c = 0;
+  for(int i = 0; i < hi;++i)
+    for(int j = i + 1;j <= hi;++j)
+      if(a[i] > a[j]) ++c;
+  return c;
 }
 
 int main()
 {
-  int a[] = {0,1,2,3,4,5,6,7,8,9};
-  int i = reverse_order(a,0,2);
-  printf("%d\n",i);
-  for(i = 0;i < len(a);++i)
-    printf("%d,",a[i]);
+  int a[100] = {0};
+  int l;
+  scanf("%d",&l);
+  for(int i = 0;i < l;++i)
+    a[i] = rand() % 100 ;
+  int  c = r(a,l - 1);
+  reverse_order(a,0,l - 1);
+  printf("c = %d \ncounts = %d\n",c,counts);
+  print_array(a,0,l - 1);
   return 0;
 }
