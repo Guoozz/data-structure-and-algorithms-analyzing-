5b009a8b72488e587e6d7bace2c69f642113c41b
Guoozz <csguozhen@gmail.com>
2015-10-05 14:46:24 +0800

    最长公共子序列

---
+++ algs/largest-common-subsequence.c
@@ -0,0 +1,58 @@
+#include<stdio.h>
+#include<stdlib.h>
+/*
+  最长公共子序列问题
+  m[i,j] 表示序列X,Y最长公共子序列的长度，其中i,j分别为X,Y中序列的数目
+  递归定义:
+  m[i,j] = 0 {if i=0 or j=0}
+  m[i,j] = m[i-1,j-1]+1 {if x[i]=x[j] and i,j>0}
+  m[i,j] = max{m[i-1,j],m[j,i-1]} {if x[i]!=x[j] and i,j>0}
+ */
+
+/* 带备忘的最长公共子序列算法 */
+
+int largest(int x[],int y[],int m[][100],int i,int j)
+{
+  if(i == 0 || j == 0)
+    return 0;
+  if(m[i][j] > 0)
+    return m[i][j];
+  if(x[i] == y[j])
+    m[i][j]= largest(x,y,m,i-1,j-1)+1;
+  else if(largest(x,y,m,i-1,j) >= largest(x,y,m,i,j-1))
+    m[i][j] = largest(x,y,m,i-1,j);
+  else
+    m[i][j] = largest(x,y,m,i,j-1);
+  return m[i][j];
+}
+
+void print_largest(int x[],int y[],int m[][100],int i,int j)
+{
+  if(i == -1 || j == -1)
+    return ;
+  if(x[i] == y[j]){
+    if(m[i][j] == m[i-1][j-1] + 1){
+      print_largest(x,y,m,i-1,j-1);
+      printf("%d\t",x[i]);
+    }
+  }
+  else if(m[i][j] == m[i-1][j])
+    print_largest(x,y,m,i-1,j);
+  else
+    print_largest(x,y,m,i,j-1);
+}
+
+int main()
+{
+  /* 求 X,Y的最长公共子序列 */
+  int x[] = {0,2,3,7,1111,333,444,43,33};
+  int y[] = {0,2,3,7,33};
+  int m[100][100];
+  for(int i = 0;i < 100;++i)
+    for(int j = 0;j < 100;++j)
+      m[i][j] = 0;
+  printf("%d\n",largest(x,y,m,8,4));
+  print_largest(x,y,m,8,4);
+  putchar('\n');
+  return 0;
+}
